ae1cf0f767f94b47953451c5ac53189c8bb96d1d
michalmaciejewski91 <michalmaciejewski91@gmail.com>
2017-11-19 09:28:28 +0100

    lab1_2 sample (instances)

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -4,8 +4,20 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
-	
-	
+  var ec2 = new AWS.EC2();
+
+  var params = {};
+  ec2.describeInstances(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      callback(err);
+    }
+    else {
+      console.log(data);
+      callback(null, data);
+    }
+  });
+
 }
 
-exports.lab = task
+exports.lab = task;
